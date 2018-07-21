7aadd9bb4bdbdcd68bb276d0a7b90ed8286361ef
Olivier Willems <ojwillems@gmail.com>
2018-07-21 00:04:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles");
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array){
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
